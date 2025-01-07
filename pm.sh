5adaec5d085840153ab598f1508991ce06ae6f93
apbianco <apbianco@gmail.com>
2025-01-07 08:52:54 +0100

    PM championship
    - RESULTS, INPUT: New globals
    - Adjust date to d/m/y format
    - Adjust font size to 8.5

--- pm.sh
+++ pm.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+RESULTS=/Users/apetitbianco/Documents/GitHub/sca/results.py
+INPUT=/USers/apetitbianco/Downloads/EDITION.txt
+
 tmp_txt=$(mktemp)
 tmp_pdf=/tmp/EDITION.pdf
 echo "// Text output in $tmp_txt"
 echo "// PDF output in $tmp_pdf"
 tmp_ps=$(mktemp)
-python3 /Users/apetitbianco/Documents/GitHub/sca/results.py > $tmp_txt && \
+python3 $RESULTS $INPUT > $tmp_txt && \
     enscript --header='Classement au %D{%d/%m/%y} %C||Page $% of $=' \
-	     --margin=10:10:10:10 -1 -f Courrier8 -e --word-wrap --media=A4 \
+	     --margin=10:10:10:10 -1 -f Courrier8.5 -e --word-wrap --media=A4 \
 	     $tmp_txt -o $tmp_ps && \
     ps2pdf $tmp_ps $tmp_pdf
